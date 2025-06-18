5aaa27784a87f7dcc9b4f096aba005e3951d3931
Soham <gsoham101gmail.com>
2025-06-18 11:36:22 +0530

    added complete theme

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -65,8 +65,8 @@ export const StarBackground = () => {
         ))}
         {meteors.map((meteor) => (
             <div key={meteor.id} className="meteor animate-meteor" style={{
-                width: meteor.size + "px",
-                height: meteor.size + "px",
+                width: meteor.size*50 + "px",
+                height: meteor.size*2 + "px",
                 left: meteor.x + "%",
                 top: meteor.y + "%",
                 animationDelay: meteor.delay ,
