d02272d2ab0ae2b996567ea437093ce786452ef5
Soham <gsoham101gmail.com>
2025-06-19 21:31:07 +0530

    final commit - everything added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,11 @@ import { Navbar } from "../components/Navbar"
 import { StarBackground } from "../components/StarBackground"
 import { ThemeToggle } from "../components/ThemeToggle"
 import { HeroSection } from "../components/HeroSection"
+import { AboutSection } from "../components/AboutSection"
+import { SkillsSection } from "../components/SkillsSection"
+import { ProjectSection } from "../components/ProjectsSection"
+import { ContactSection } from "../components/ContactSection"
+import { Footer } from "../components/Footer"
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden"> 
@@ -15,8 +20,12 @@ export const Home = () => {
         {/* Main Content */}
         <main>
             <HeroSection />
+            <AboutSection />
+            <SkillsSection />
+            <ProjectSection />
+            <ContactSection />
         </main>
         {/* Footer */}
-
+            <Footer />
     </div>
 }
